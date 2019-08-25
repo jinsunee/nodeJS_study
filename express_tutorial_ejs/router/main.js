7ab756883faa1f9f09a0e95b361755c6aabb23ce
ilikeu7246 <jsunee7246@gmail.com>
2019-08-25 15:40:55 +0900

    add get, post

--- express_tutorial_ejs/router/main.js
+++ express_tutorial_ejs/router/main.js
@@ -7,6 +7,55 @@ module.exports = function(app, fs){
             length : 5
         });
     });
+
+    app.get('/list', function(req, res){
+        fs.readFile(__dirname + "/../data/" + "user.json", 'utf8', function(err, data){
+            console.log(data);
+            res.end(data);
+        });
+    });
+
+    app.get('/getUser/:username', function(req, res){
+        fs.readFile( __dirname + "/../data/user.json", 'utf8', function(err, data){
+            var users = JSON.parse(data);
+            res.json(users[req.params.username]);
+        })
+    });
+
+    app.post('/addUser/:username', function(req, res){
+        var result = { };
+        var username = req.params.username;
+
+        //check req validity
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        // load data & check duplication
+        fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data){
+            var users = JSON.parse(data);
+            if(users[username]){
+                //duplication found
+                result["success"] = 0;
+                result["error"] = "duplicate";
+                res.json(result);
+                return;
+            }
+
+            // add to data
+            users[username] = req.body;
+
+            //save data
+            fs.writeFile(__dirname + "/../data/user.json",
+                        JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                    result = {"success" : 1};
+                    res.json(result);
+                })
+        })
+    })
 }
 
 //router코드를 따로 작성해서 내보냈기 때문에 server.js에서 모듈로서 불러올 수 있게 됌.
