c93cce85b7306ec876a55b651dadd309241025ef
ilikeu7246 <jsunee7246@gmail.com>
2019-08-25 15:01:00 +0900

    complete ejs example

--- express_tutorial_ejs/router/main.js
+++ express_tutorial_ejs/router/main.js
@@ -1,10 +1,11 @@
 //Router를 모듈화해서 exports함.
 module.exports = function(app, fs){
     app.get('/', function(req, res){
-        res.render('index.html');
-    });
-    app.get('/about', function(req, res){
-        res.render('about.html');
+        //render함수의 두번째 인자로 json데이터를 전달함으로써 페이지에서 데이터를 사용가능하게 된다.
+        res.render('index', {
+            title : "MY HOMEPAGE",
+            length : 5
+        });
     });
 }
 
